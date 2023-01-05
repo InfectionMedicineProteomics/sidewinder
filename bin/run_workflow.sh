e9f043ab92e7fbaa571ec9b24c259e1d92e902a0
Joel Ströbaek <joel.strobaek@med.lu.se>
2023-01-05 12:21:48 +0100

    Update run-script

--- bin/run_workflow.sh
+++ bin/run_workflow.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+output_dir=$(grep -E '^output_dir: ' config/config.yml | cut -f2 -d' ' | tr -d "'")
 
-snakemake -c 10 --use-singularity --use-conda "$@"
+snakemake --use-singularity --singularity-args "--bind $output_dir:/data" --use-conda "$@"
