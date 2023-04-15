a66d33cf11b14ac34908c841dcce3ea0601c79f1
Joel Ströbaek <joel.strobaek@med.lu.se>
2023-04-15 11:08:47 +0200

    Update workflow execution to current state

--- bin/run_workflow.sh
+++ bin/run_workflow.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-output_dir=$(grep -E '^output_dir: ' config/user_config.yml | cut -f2 -d' ' | tr -d "'")
+#output_dir=$(grep -E '^output_dir: ' config/user_config.yml | cut -f2 -d' ' | tr -d "'")
 
-snakemake --use-singularity --singularity-args "--bind $output_dir:/data" --use-conda --cores -k "$@"
+# Removed from command:
+# --singularity-args "--bind $output_dir:/data"
+
+snakemake --use-singularity --use-conda --cores -k "$@"
